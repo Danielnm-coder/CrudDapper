80a6de0ea6ddae37a7d038c14d070be6bdaaafd3
Danielnm-coder <dn644625@gmail.com>
2024-10-07 12:48:36 -0300

    first commit

---
+++ ProjetoAula03/Settings/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula03.Settings
+{
+    /// <summary>
+    /// Classe para congigurar parametros ultilizados pelo projeto
+    /// </summary>
+    public class AppSettings
+    {
+        /// <summary>
+        /// Propriedades para retornar a string de conexão do bando de dados 
+        /// </summary>
+        public static string ConnectionString
+        {
+            get {
+                return "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDAula03;Integrated Security=True";
+            }
+
+        }
+    }
+}
